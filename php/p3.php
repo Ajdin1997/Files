a4d203c3077f71ac29f47b34e3c6cebe53da0a80
Ajdin Alic <alicajdin_97_@hotmail.com>
2015-08-26 20:08:03 +0200

    Update p3.php

--- php/p3.php
+++ php/p3.php
@@ -98,6 +98,9 @@
                 "border-left": "rgba(174, 69, 189, 1) 3px solid !important",
                 "background-color": "rgba(190, 28, 213, 0.22) !important"
             },
+            "#chat.message.from-3625731 -from": {
+                 "pointer-events": "none"
+             },
             "#chat-messages .emote .icon": {
                 "left": "4px"
             },
