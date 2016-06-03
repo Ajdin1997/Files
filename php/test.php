43249411e2d7ca960e70ffa9d09dd9209d7719fa
Ajdin Alic <alicajdin_97_@hotmail.com>
2016-06-03 20:01:23 +0200

    Create test.php

---
+++ php/test.php
@@ -0,0 +1,86 @@
+{
+    "css": {
+        "font": [
+        ],
+        "import": [
+        ],
+        "rule": {
+             "#footer": {
+                "background": "#121212;",
+                 "border-top": "1px solid blue;"
+            },
+            "#playlist-button": {
+                "background": "#DF013A;"
+            },
+            "#dj-button": {
+                "background": "#121212;",
+                "color": "#DF013A;"
+            },
+            ".left": {
+                "background": "#0A0A0A;"
+            },
+            "#woot .bottom, #woot.selected": {
+                "background": "background: #D02490;"
+            },
+            "#meh .bottom, #meh.selected": {
+                "background": "#FE2E2E;"
+            },
+            "#room-host, #room-name": {
+                "text-transform": "uppercase;",
+                "color": "yellow;"
+            },
+            ".app-header": {
+                "background": "#121212;",
+                "border-bottom": "2px solid blue;"
+            },
+            "#history-button": {
+                "background": "#5A071C;"
+            },
+            "#app-menu, #app-menu .list": {
+                "background": "#5A071C;"
+            },
+            "#footer-user .buttons": {
+                "background": "#05051F;"
+            },
+            "#chat .emote": {
+                "border-left": "rgba(174, 69, 189, 1) 3px solid !important",
+                "background-color": "rgba(190, 28, 213, 0.22) !important"
+            },
+            ".message.from-3625731 .from": {
+                 "pointer-events": "none !important" 
+             },
+            "#chat-messages .emote .icon": {
+                "left": "4px"
+            },
+            "#chat .emote .text": {
+                "font-style": "normal",
+                "font-weight": "bold",
+                "color": "#ac76ff"
+            },
+             "#chat .emote.from-3625731 .text": {
+                "font-style": "normal",
+                "font-weight": "bold",
+                "color": "#ac76ff"
+            }
+              
+        }
+
+    },
+    "images": {
+        "background": "http://i.imgur.com/fESBojm.png",
+        "booth": "",
+        "playback": "http://i.imgur.com/toCoya3.jpg",
+    "icons":{
+         "bouncer":"",
+         "cohost":"",
+         "host":"",
+         "manager":"",
+         "residentdj":""
+      }
+    },
+    "rules": {
+        "allowAutorespond": "true",
+        "allowAutowoot": "true",
+        "allowAutojoin": "true"
+    }
+}
