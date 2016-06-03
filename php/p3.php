2abca023c1691264bc027275038f3a61a4aaf084
Ajdin Alic <alicajdin_97_@hotmail.com>
2016-06-03 20:08:12 +0200

    Update p3.php

--- php/p3.php
+++ php/p3.php
@@ -6,93 +6,41 @@
         ],
         "rule": {
              "#footer": {
-                "background": "linear-gradient(to bottom,#180000   0,#180000  90%)"
+                "background": "#121212;",
+                 "border-top": "1px solid blue;"
             },
             "#playlist-button": {
-                "background": "linear-gradient(to bottom,#FF5CDE 0,#5700FF 100%)"
-            },
-            "#header": {
-                "background": "linear-gradient(to bottom,#000000 0,#000000 90%)"
-            },
-            "#store-button": {
-                "background": "linear-gradient(to bottom,#FF0000 0,#A00000 100%)"
-            },
-            "#plug-dj": {
-                "background": "linear-gradient(to bottom,#0268AC 0,#02225C 100%)"
-            },
-            ".header-panel-button.selected": {
-                "background": "linear-gradient(to bottom,rgba(255, 255, 255, 0.25) 0,rgba(255, 255, 255, 0.06) 100%)"
-            },
-            "#plug-menu": {
-                "background": "rgba(40, 44, 53, 0.94) !important"
-            },
-            "#plug-menu .legal": {
-                "background": "rgba(68, 74, 89, 0.46) !important"
-            },
-            "#settings": {
-                "background": "rgba(10, 10, 10, 0.89) !important"
-            },
-            "#settings .header": {
-                "background": "rgba(28, 31, 37, 0.78) !important"
-            },
-            "#chat": {
-                "background": "linear-gradient(to bottom,#000000 0,#000000 90%)"
+                "background": "#DF013A;"
             },
             "#dj-button": {
-                "background": "rgba(40, 44, 53, 0.75) !important"
+                "background": "#121212;",
+                "color": "#DF013A;"
             },
-            "#dj-button .left": {
-                "background": "linear-gradient(to bottom,rgba(0, 181, 230, 0.75) 0,rgba(0, 156, 221, 0.75) 100%)"
+            ".left": {
+                "background": "#0A0A0A;"
             },
-            "#playlist-panel": {
-                "background": "rgba(17, 19, 23, 0.9)"
+            "#woot .bottom, #woot.selected": {
+                "background": "background: #D02490;"
             },
-            "#playlist-menu": {
-                "background": "rgba(28, 31, 37, 0.67)"
+            "#meh .bottom, #meh.selected": {
+                "background": "#FE2E2E;"
             },
-            "#search": {
-                "background": "rgba(50, 55, 66, 0.58)"
+            "#room-host, #room-name": {
+                "text-transform": "uppercase;",
+                "color": "yellow;"
             },
-            "#search-bar": {
-                "background": "rgba(17, 19, 23, 0.77)"
+            ".app-header": {
+                "background": "#121212;",
+                "border-bottom": "2px solid blue;"
             },
-            "#filter-bar": {
-                "background": "rgba(17, 19, 23, 0.77)"
+            "#history-button": {
+                "background": "#5A071C;"
             },
-            "#playlist-edit-button": {
-                "background": "rgba(85, 93, 112, 0.75)"
+            "#app-menu, #app-menu .list": {
+                "background": "#5A071C;"
             },
-            "#playlist-shuffle-button": {
-                "background": "rgba(68, 74, 89, 0.75)"
-            },
-            "#playlist-delete-button": {
-                "background": "rgba(50, 55, 66, 0.75)"
-            },
-            "#user-panel .score": {
-                "background": "rgba(28, 31, 37, 0.59)"
-            },
-            "#dj-button.is-locked .left,#dj-button.is-full .left,#vote .crowd-response.disabled .bottom": {
-                "background": "rgba(68, 74, 89, 0.75)"
-            },
-            "#dj-button.is-quit .left,#dj-button.is-leave .left,#playlist-delete-button:hover": {
-                "background": "linear-gradient(to bottom,rgba(244, 107, 64, 0.74) 0,rgba(240, 79, 48, 0.75) 100%)"
-            },
-            "#chat .message:nth-child(2n+1),#chat .mention:nth-child(2n+1),#chat .skip:nth-child(2n+1),#chat .moderation:nth-child(2n+1),#chat .emote:nth-child(2n+1),#chat .update:nth-child(2n+1),#chat .welcome:nth-child(2n+1)": {
-                "background-color": "rgba(17, 19, 23, 0.75)"
-            },
-            "#chat .emote.from-4573622": {
-                "border-left": "#ac76ff 3px solid",
-                "background-color": "rgba(24, 0, 38, 0.9) !important"
-            
-            },
-            ".emote.from-4573622 .from.staff.clickable": {
-                "display": "none"
-            },
-            ".emote.from-4573622 .icon-chat-manager": {
-                "display": "none"
-            },
-            "#chat .emote.from-4573622 .from": {
-                 "color": "#ac76ff !important"
+            "#footer-user .buttons": {
+                "background": "#05051F;"
             },
             "#chat .emote": {
                 "border-left": "rgba(174, 69, 189, 1) 3px solid !important",
@@ -109,7 +57,7 @@
                 "font-weight": "bold",
                 "color": "#ac76ff"
             },
-             "#chat .emote.from-4573622 .text": {
+             "#chat .emote.from-3625731 .text": {
                 "font-style": "normal",
                 "font-weight": "bold",
                 "color": "#ac76ff"
@@ -119,7 +67,7 @@
 
     },
     "images": {
-        "background": "http://i.imgur.com/fESBojm.png",
+        "background": "http://i.imgur.com/u36VR4n.png",
         "booth": "",
         "playback": "http://i.imgur.com/toCoya3.jpg",
     "icons":{
@@ -133,6 +81,6 @@
     "rules": {
         "allowAutorespond": "true",
         "allowAutowoot": "true",
-	    "allowAutojoin": "true"
+        "allowAutojoin": "true"
     }
 }
