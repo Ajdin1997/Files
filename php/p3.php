e0bea5b7b83430af996cc236c1262b2b5da5a8c7
Ajdin Alic <alicajdin_97_@hotmail.com>
2016-07-01 14:59:58 +0200

    Update p3.php

--- php/p3.php
+++ php/p3.php
@@ -5,63 +5,6 @@
         "import": [
         ],
         "rule": {
-             "#footer": {
-                "background": "#121212;",
-                 "border-top": "1px solid blue;"
-            },
-            "#room-bar .icon-room": {
-            "background-image": "url("http://i.imgur.com/ekvxgju.png");",
-            "top": "11px;",
-            "left": "7px;",
-            },
-            "#playlist-button": {
-                "background": "#DF013A;"
-            },
-            "#dj-button": {
-                "background": "#121212;",
-                "color": "#DF013A;"
-            },
-            ".left": {
-                "background": "#0A0A0A;"
-            },
-            "#woot .bottom, #woot.selected": {
-                "background": "background: #D02490;"
-            },
-            "#meh .bottom, #meh.selected": {
-                "background": "#FE2E2E;"
-            },
-            "#room-host, #room-name": {
-                "text-transform": "uppercase;",
-                "color": "yellow;"
-            },
-            ".app-header": {
-                "background": "#121212;",
-                "border-bottom": "2px solid blue;"
-            },
-            "#history-button": {
-                "background": "#5A071C;"
-            },
-            "#app-menu, #app-menu .list": {
-                "background": "#121212;"
-            },
-            "#footer-user .buttons": {
-                "background": "#05051F;"
-            },
-            "#playlist-menu": {
-                "background": "#0E121B;",
-                "border-right": "1px solid blue;"
-            },
-            "#playlist-menu .button": {
-               "background": "#673AB7;"
-            },
-            "#user-menu": {
-               "background": "#121212;",
-                "border-right": "1px solid blue;"
-            },
-            "#room-settings-menu": {
-               "background": "#0E121B;",
-               "border-right": "1px solid blue"
-            },
             "#chat .emote": {
                 "border-left": "rgba(174, 69, 189, 1) 3px solid !important",
                 "background-color": "rgba(190, 28, 213, 0.22) !important"
@@ -85,18 +28,6 @@
               
         }
 
-    },
-    "images": {
-        "background": "http://i.imgur.com/u36VR4n.png",
-        "booth": "",
-        "playback": "http://i.imgur.com/toCoya3.jpg",
-    "icons":{
-         "bouncer":"",
-         "cohost":"",
-         "host":"",
-         "manager":"",
-         "residentdj":""
-      }
     },
     "rules": {
         "allowAutorespond": "true",
